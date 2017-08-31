2a6e88dc0fd5247d7f4afed9c88b9b7eb2df72e5
Felix Meyer-Wolters <felix@egoversum.com>
2017-08-31 10:35:20 +0200

    Refactor publish.py

--- publish.py
+++ publish.py
@@ -21,29 +21,35 @@ def get_version_from_cfg():
 
 def publish() -> None:
     """"""
+
+    publish_seq_args_1 = publish_seq.get_args_interactively(
+        run_update_version='y',
+        run_build_file='y',
+        build_file=build_file,
+        cur_version=get_version_from_cfg(),
+    )
+
+    results_1 = [*publish_seq.run_seq(**publish_seq_args_1)]
+
     git_seq_args = git_seq.get_args_interactively(
         run_any='y',
         confirm_status='y',
         confirm_diff='y',
-        run_update_version='y',
         run_add_all='y',
         run_commit='y',
         run_tag='y',
         run_push='y',
-        cfg_file=cfg_file,
-        cur_version=get_version_from_cfg(),
-        )
+        new_version=publish_seq_args_1.get('version') or get_version_from_cfg()
+    )
 
-    publish_seq_args = publish_seq.get_args_interactively(
-        run_build_file='y',
+    publish_seq_args_2 = publish_seq.get_args_interactively(
         run_push_pypi='y',
         cfg_file=cfg_file,
-        build_file=build_file,
         wheel_dir=wheel_dir,
-        new_version=git_seq_args.get('version') or get_version_from_cfg()
-        )
+        new_version=publish_seq_args_1.get('version') or get_version_from_cfg()
+    )
 
-    results = [*git_seq.run_seq(**git_seq_args), *publish_seq.run_seq(**publish_seq_args)]
+    results = [*git_seq.run_seq(**git_seq_args), *publish_seq.run_seq(**publish_seq_args_2)]
 
     print(h3('Publish Results'))
 
