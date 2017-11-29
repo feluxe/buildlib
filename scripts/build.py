abedf32d2d8e3e51b5cef32094daa9c52d6162ac
Felix Meyer-Wolters <felix@egoversum.com>
2017-11-29 20:12:28 +0100

    Add git sequence.

--- scripts/build.py
+++ scripts/build.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath(os.path.join('..', 'buildlib')))
 from typing import Optional, List
 from buildlib.utils import yaml
 from buildlib.cmds import semver, git, build, pipenv
+from buildlib.cmds.git import sequence as git_seq
 from cmdinter import CmdFuncResult
 
 CWD = os.getcwd()
@@ -33,100 +34,6 @@ def bump_version(version: str) -> CmdFuncResult:
         config_file=CFG_FILE,
         semver_num=version,
     )
-
-
-class GitBumpSettings(dict):
-    version: str
-    should_bump_git: Optional[bool]
-    should_add_all: Optional[bool]
-    should_commit: Optional[bool]
-    commit_msg: Optional[str]
-    should_tag: Optional[bool]
-    should_push_git: Optional[bool]
-    branch: Optional[str]
-
-
-def get_git_settings_from_user(
-    version: str,
-    should_tag_default: Optional[bool],
-) -> GitBumpSettings:
-    """"""
-    s = GitBumpSettings()
-    s.version = version
-
-    # Ask user to run any git commands.
-    s.should_bump_git: bool = git.prompt.confirm_status('y') \
-                              and git.prompt.confirm_diff('y')
-
-    # Git routine
-    if s.should_bump_git:
-        # Ask user to run 'git add -A.
-        s.should_add_all: bool = git.prompt.should_add_all(
-            default='y'
-        )
-
-        # Ask user to run commit.
-        s.should_commit: bool = git.prompt.should_commit(
-            default='y'
-        )
-
-        # Get commit msg from user.
-        if s.should_commit:
-            s.commit_msg: str = git.prompt.commit_msg()
-
-        # Ask user to run 'tag'.
-        s.should_tag: bool = git.prompt.should_tag(
-            default='n' if should_tag_default is False else 'y'
-        )
-
-        # Ask user to push.
-        s.should_push_git: bool = git.prompt.should_push(
-            default='y'
-        )
-
-        # Ask user for branch.
-        if any([
-            s.should_tag,
-            s.should_push_git
-        ]):
-            s.branch: str = git.prompt.branch()
-
-    return s
-
-
-def bump_git(
-    s: GitBumpSettings
-) -> List[CmdFuncResult]:
-    """"""
-    results = []
-
-    # If any git commands should be run.
-    if s.should_bump_git:
-        # Run 'add -A'
-        if s.should_add_all:
-            results.append(
-                git.add_all()
-            )
-
-        # Run 'commit -m'
-        if s.should_commit:
-            results.append(
-                git.commit(s.commit_msg)
-            )
-
-        # Run 'tag'
-        if s.should_tag:
-            results.append(
-                git.tag(s.version, s.branch)
-            )
-
-        # Run 'push'
-        if s.should_push_git:
-            results.append(
-                git.push(s.branch)
-            )
-
-    return results
 
 
 def build_wheel_routine() -> None:
@@ -164,15 +71,16 @@ def bump_git_routine() -> None:
     else:
         version = CFG.get('version')
 
-    git_settings = get_git_settings_from_user(
+    git_settings = git_seq.get_sequence_settings_from_user(
         should_tag_default=version != CFG.get('version'),
+        should_bump_any=True,
         version=version,
     )
 
     if should_bump_version:
         results.append(bump_version(version))
 
-    results += bump_git(git_settings)
+    results += git_seq.bump_sequence(git_settings)
 
     print('')
 
@@ -204,7 +112,7 @@ def bump_routine() -> None:
     if should_bump_version:
         results.append(bump_version(version))
 
-    git_settings = get_git_settings_from_user(
+    git_settings = git_seq.get_sequence_settings_from_user(
         should_tag_default=version != CFG.get('version'),
         version=version,
     )
@@ -212,8 +120,8 @@ def bump_routine() -> None:
     if should_build_wheel:
         results.append(build.build_python_wheel(clean_dir=True))
 
-    if git_settings.should_bump_git:
-        results += bump_git(git_settings)
+    if git_settings.should_bump_any:
+        results += git_seq.bump_sequence(git_settings)
 
     if should_push_registry:
         results.append(build.push_python_wheel_to_pypi(
