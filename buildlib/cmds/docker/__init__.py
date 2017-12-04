1d00df4fdc17a10f42fbb1932d7452e7cc4cee08
Felix Meyer-Wolters <felix@egoversum.com>
2017-12-04 13:10:26 +0100

    Add more options to docker commands

--- buildlib/cmds/docker/__init__.py
+++ buildlib/cmds/docker/__init__.py
@@ -14,31 +14,35 @@ def _image_exists(image) -> bool:
     return 'Error: No such image' not in result.stdout
 
 
+def _parse_args(args: List, flag):
+    if args:
+        nested = [[flag, f] for f in args]
+        return reduce(lambda x, y: x + y, nested)
+    else:
+        return []
+
+
 def run_container(
     image: str,
     publish: Optional[List[str]] = None,
     add_host: Optional[List[str]] = None,
+    volume: Optional[List[str]] = None,
+    env: Optional[List[str]] = None,
 ) -> CmdFuncResult:
     """
     Run Docker container locally.
     """
     title = 'Run Docker Container.'
 
-    if add_host:
-        nested = [['--add-host', h] for h in add_host]
-        add_host = reduce(lambda x, y: x + y, nested)
-    else:
-        add_host = []
-
-    if publish:
-        nested = [['-p', p] for p in publish]
-        publish = reduce(lambda x, y: x + y, nested)
-    else:
-        publish = []
+    add_host = _parse_args(add_host, '--add-host')
+    env = _parse_args(env, '-e')
+    publish = _parse_args(publish, '-p')
+    volume = _parse_args(volume, '-v')
 
     # p = run(['docker', 'run', '-d', '-p', f'127.0.0.1:{port}:{port}', image])
 
-    p = run(['docker', 'run', '-d'] + add_host + publish + [image])
+    p = run(
+        ['docker', 'run', '-d'] + add_host + env + publish + volume + [image])
 
     if p.returncode == 0:
         status: str = Status.ok
@@ -121,11 +125,7 @@ def build_image(
     """
     title = 'Build Docker Image.'
 
-    if build_arg:
-        nested = [['--build-arg', arg] for arg in build_arg]
-        build_arg = reduce(lambda x, y: x + y, nested)
-    else:
-        build_arg = []
+    build_arg = _parse_args(build_arg, '--build-arg')
 
     cmd = ['docker', 'build', '.', '--pull', '-f', 'Dockerfile'] + build_arg
 
