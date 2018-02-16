9b482538b1036b7a392443bbdd674b52032ee6e0
feluxe <felix@meyerwolters.de>
2018-02-16 21:10:30 +0100

    Update builder tool.

--- DEVNOTES.md
+++ DEVNOTES.md
@@ -1,7 +1,3 @@
 
 # TODO
 
-### [] Add function to remove egg after build.
-### [x] Add functionality to upload package to pypi.
-### [x] Release stable version on pypi.
-### [x] Create git repo.
--- builder/__init__.py
+++ builder/__init__.py
@@ -1,11 +1,11 @@
 import sys
 import os
+from typing import Union
+from cmdi import CmdResult, print_summary
 
 sys.path.append(os.path.abspath(os.path.join('..', 'buildlib')))
 
-from typing import Union
 from buildlib import yaml, semver, build, git
-from cmdi import CmdResult, print_summary
 
 CFG_FILE = 'Project'
 CFG = yaml.load(
@@ -16,20 +16,11 @@ CFG = yaml.load(
 __version__ = CFG.get('version')
 
 
-def get_version_from_user() -> str:
-    """
-    Get new Version number from user or use the one from CONFIG.yaml.
-    """
-    return semver.prompt.semver_num_by_choice(
-        cur_version=CFG.get('version')
-    )
-
-
 def build_wheel(
 ) -> Union[CmdResult, None]:
     """"""
     return build.cmd.build_python_wheel(
-        clean_dir=True
+        clean_dir=True,
     )
 
 
@@ -37,18 +28,17 @@ def push_registry(
 ) -> Union[CmdResult, None]:
     """"""
     return build.cmd.push_python_wheel_to_pypi(
-        clean_dir=True
+        clean_dir=True,
     )
 
 
-def bump_version(
-    new_version: str = None,
-) -> Union[CmdResult, None]:
+def bump_version() -> Union[CmdResult, None]:
     """
     Bump (update) version number in CONFIG.yaml.
     """
-    if not new_version:
-        new_version: str = get_version_from_user()
+    new_version: str = semver.prompt.semver_num_by_choice(
+        cur_version=CFG.get('version')
+    )
 
     return build.cmd.update_version_num_in_cfg(
         config_file=CFG_FILE,
@@ -59,26 +49,19 @@ def bump_version(
 def bump_git() -> None:
     """"""
     results = []
+    version_bumped = False
 
-    should_bump_version = build.prompt.should_update_version(
-        default='y'
-    )
-
-    if should_bump_version:
-        version = get_version_from_user()
-    else:
-        version = CFG.get('version')
+    if build.prompt.should_update_version(
+        default='y',
+    ):
+        bump_version()
+        version_bumped = True
 
     seq_settings = git.seq.get_settings_from_user(
-        should_tag_default=version != CFG.get('version'),
+        should_tag_default=version_bumped,
         should_bump_any=True,
-        version=version,
+        version=CFG.get('version'),
     )
-
-    if should_bump_version:
-        results.append(bump_version(
-            new_version=version,
-        ))
 
     results.extend(git.seq.bump_sequence(seq_settings))
 
@@ -88,32 +71,25 @@ def bump_git() -> None:
 def bump_all() -> None:
     """"""
     results = []
+    version_was_bumped = False
 
-    should_bump_version: bool = build.prompt.should_update_version(
+    if build.prompt.should_update_version(
         default='y'
-    )
-
-    if should_bump_version:
-        version = get_version_from_user()
-    else:
-        version = CFG.get('version')
+    ):
+        bump_version()
+        version_was_bumped = True
 
     should_build_wheel: bool = build.prompt.should_build_wheel(
-        default='y'
+        default='y',
     )
 
     should_push_registry: bool = build.prompt.should_push_pypi(
-        default='y' if should_bump_version else 'n'
+        default='y' if version_was_bumped else 'n',
     )
 
-    if should_bump_version:
-        results.append(bump_version(
-            new_version=version,
-        ))
-
     git_settings = git.seq.get_settings_from_user(
-        should_tag_default=version != CFG.get('version'),
-        version=version,
+        should_tag_default=version_was_bumped,
+        version=CFG.get('version'),
     )
 
     if should_build_wheel:
@@ -126,7 +102,7 @@ def bump_all() -> None:
 
     if should_push_registry:
         results.append(build.cmd.push_python_wheel_to_pypi(
-            clean_dir=True
+            clean_dir=True,
         ))
 
     print_summary(results)
