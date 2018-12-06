38fe89899f68095f2a42e6a11d50668df9b3ab51
feluxe <felix@meyerwolters.de>
2018-12-06 19:03:02 +0100

    Test deployment. Fix bug with module/package structure

--- buildlib/buildmisc/__init__.py
+++ buildlib/buildmisc/__init__.py
@@ -3,6 +3,5 @@ Misc build commands.
 Everything that does not deserve it's own package goes here.
 """
 
-from ..buildmisc import _prompt as prompt
 from ..buildmisc._lib import *
 from ..buildmisc import _lib_cmd as cmd
